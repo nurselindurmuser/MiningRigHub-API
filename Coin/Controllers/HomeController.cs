ce068f051f998016331ae7d316644997b5ed41a5
Nurselin Durmuşer <nurselin.durmuser@gmail.com>
2018-12-11 17:42:55 +0300

    v1

---
+++ Coin/Controllers/HomeController.cs
@@ -0,0 +1,72 @@
+﻿using Coin.Models;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Coin.Controllers
+{
+    public class HomeController : Controller
+    {
+
+        HttpClient client = new HttpClient();
+        public async Task<ActionResult> Index()
+        {
+            CoinBorsa coinBorsa = null;
+            HttpResponseMessage response = await client.GetAsync("https://koineks.com/ticker");
+            if (response.IsSuccessStatusCode)
+            {
+                var json = await response.Content.ReadAsStringAsync();
+                coinBorsa = JsonConvert.DeserializeObject<CoinBorsa>(json);
+            }
+            ViewBag.coinBorsa = coinBorsa;
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+        public ActionResult Blog()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+        public ActionResult Crypto()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+        public ActionResult Comments()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+        public ActionResult BlogSingle()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
---
+++ Coin/Models/CoinBorsa.cs
@@ -0,0 +1,246 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Coin.Models
+{
+    //BTC
+    public class BTC
+    {
+        public string short_code;
+        public string name;
+        public string currency;
+        public string current;
+        public string change_amount;
+        public string change_percentage;
+        public string high;
+        public string low;
+        public string volume;
+        public string ask;
+        public string bid;
+        public string timestamp;
+    }
+
+    //ETH
+    public class ETH
+    {
+        public string short_code;
+        public string name;
+        public string currency;
+        public string current;
+        public string change_amount;
+        public string change_percentage;
+        public string high;
+        public string low;
+        public string volume;
+        public string ask;
+        public string bid;
+        public string timestamp;
+    }
+
+    //TRX
+    public class TRX
+    {
+        public string short_code;
+        public string name;
+        public string currency;
+        public string current;
+        public string change_amount;
+        public string change_percentage;
+        public string high;
+        public string low;
+        public string volume;
+        public string ask;
+        public string bid;
+        public string timestamp;
+    }
+
+    //USDT
+    public class USDT
+    {
+        public string short_code;
+        public string name;
+        public string currency;
+        public string current;
+        public string change_amount;
+        public string change_percentage;
+        public string high;
+        public string low;
+        public string volume;
+        public string ask;
+        public string bid;
+        public string timestamp;
+    }
+
+    //BCH
+    public class BCH
+    {
+        public string short_code;
+        public string name;
+        public string currency;
+        public string current;
+        public string change_amount;
+        public string change_percentage;
+        public string high;
+        public string low;
+        public string volume;
+        public string ask;
+        public string bid;
+        public string timestamp;
+    }
+
+    //LTC
+    public class LTC
+    {
+        public string short_code;
+        public string name;
+        public string currency;
+        public string current;
+        public string change_amount;
+        public string change_percentage;
+        public string high;
+        public string low;
+        public string volume;
+        public string ask;
+        public string bid;
+        public string timestamp;
+    }
+
+    //DOGE
+    public class DOGE
+    {
+        public string short_code;
+        public string name;
+        public string currency;
+        public string current;
+        public string change_amount;
+        public string change_percentage;
+        public string high;
+        public string low;
+        public string volume;
+        public string ask;
+        public string bid;
+        public string timestamp;
+    }
+
+    //DASH
+    public class DASH
+    {
+        public string short_code;
+        public string name;
+        public string currency;
+        public string current;
+        public string change_amount;
+        public string change_percentage;
+        public string high;
+        public string low;
+        public string volume;
+        public string ask;
+        public string bid;
+        public string timestamp;
+    }
+
+    //XRP
+    public class XRP
+    {
+        public string short_code;
+        public string name;
+        public string currency;
+        public string current;
+        public string change_amount;
+        public string change_percentage;
+        public string high;
+        public string low;
+        public string volume;
+        public string ask;
+        public string bid;
+        public string timestamp;
+    }
+
+    //XLM
+    public class XLM
+    {
+        public string short_code;
+        public string name;
+        public string currency;
+        public string current;
+        public string change_amount;
+        public string change_percentage;
+        public string high;
+        public string low;
+        public string volume;
+        public string ask;
+        public string bid;
+        public string timestamp;
+    }
+
+    //XEM
+    public class XEM
+    {
+        public string short_code;
+        public string name;
+        public string currency;
+        public string current;
+        public string change_amount;
+        public string change_percentage;
+        public string high;
+        public string low;
+        public string volume;
+        public string ask;
+        public string bid;
+        public string timestamp;
+    }
+
+    //BTG
+    public class BTG
+    {
+        public string short_code;
+        public string name;
+        public string currency;
+        public string current;
+        public string change_amount;
+        public string change_percentage;
+        public string high;
+        public string low;
+        public string volume;
+        public string ask;
+        public string bid;
+        public string timestamp;
+    }
+
+    //ETC
+    public class ETC
+    {
+        public string short_code;
+        public string name;
+        public string currency;
+        public string current;
+        public string change_amount;
+        public string change_percentage;
+        public string high;
+        public string low;
+        public string volume;
+        public string ask;
+        public string bid;
+        public string timestamp;
+    }
+
+
+    public class CoinBorsa
+        {
+        public BTC BTC;
+        public ETH ETH;
+        public TRX TRX;
+        public USDT USDT;
+        public BCH BCH;
+        public LTC LTC;
+        public DOGE DOGE;
+        public DASH DASH;
+        public XRP XRP;
+        public XLM XLM;
+        public XEM XEM;
+        public BTG BTG;
+        public ETC ETC;
+    }
+}
